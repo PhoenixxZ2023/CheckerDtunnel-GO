1782995b016199ac4213cfed3720c8ac872e9519
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-10 18:53:38 -0300

    Update count_connections.go

--- src/domain/usecase/user/count_connections.go
+++ src/domain/usecase/user/count_connections.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/DTunnel0/CheckUser-Go/src/domain/contract"
+	"github.com/PhoenixxZ2023/CheckerDtunnel-GO/src/domain/contract"
 )
 
 type CountConnectionsUseCase struct {
