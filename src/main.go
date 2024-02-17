894f36aaeaa996905ccd3c3e57120688e1451ba2
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-16 23:56:51 -0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -6,16 +6,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/DTunnel0/CheckUser-Go/src/data/repository"
-	"github.com/DTunnel0/CheckUser-Go/src/infra/factory"
-	"github.com/DTunnel0/CheckUser-Go/src/infra/http"
+	"github.com/PhoenixxZ2023/CheckerDtunnel-GO/src/data/repository"
+	"github.com/PhoenixxZ2023/CheckerDtunnel-GO/src/infra/factory"
+	"github.com/PhoenixxZ2023/CheckerDtunnel-GO/src/infra/http"
 )
 
 var (
 	version     = "0.1.9"
 	version_str = fmt.Sprintf("checkuser %s", version)
-	author      = "Glemison C. Dutra"
-	email       = "glemyson20@gmail.com"
+	author      = "Turbonet Vpn 2023"
+	email       = "phoenixxz@gmail.com"
 	description = fmt.Sprintf(
 		"DTChecker - CHECKUSER | BY %s <%s> | VERSION: %s",
 		author, email, version,
